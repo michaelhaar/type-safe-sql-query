133b58caa57c9c75bf7eb32259c65f72867b4e78
Michael Haar <michael.haar92@gmail.com>
2024-01-22 11:13:35 +0100

    implement first draft of `InferReturnTypeFromSelect`

--- src/select.test-d.ts
+++ src/select.test-d.ts
@@ -1,5 +1,23 @@
 import { assertType, test, describe } from "vitest";
-import type { ExtractTableName, GetTableType } from "./select";
+import type { ExtractTableName, InferReturnTypeFromSelect, GetTableType } from "./select";
+
+type TestTables = {
+  users: {
+    id: number;
+    name: string;
+  };
+  posts: {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  };
+  comments: {
+    id: number;
+    body: string;
+    postId: number;
+  };
+};
 
 describe("ExtractTableName", () => {
   function getTableName<Query extends string>(query: Query): ExtractTableName<Query> {
@@ -28,25 +46,7 @@ describe("ExtractTableName", () => {
 });
 
 describe("GetTableType", () => {
-  type Tables = {
-    users: {
-      id: number;
-      name: string;
-    };
-    posts: {
-      id: number;
-      title: string;
-      body: string;
-      userId: number;
-    };
-    comments: {
-      id: number;
-      body: string;
-      postId: number;
-    };
-  };
-
-  function getTableType<Query extends string>(query: Query): GetTableType<Query, Tables> {
+  function getTableType<Query extends string>(query: Query): GetTableType<Query, TestTables> {
     return query as any;
   }
 
@@ -65,6 +65,29 @@ describe("GetTableType", () => {
       title: string;
       body: string;
       userId: number;
+    }>(tableName);
+  });
+});
+
+describe("InferReturnTypeFromSelect", () => {
+  function getReturnTypeFromSelect<Query extends string>(query: Query): InferReturnTypeFromSelect<Query, TestTables> {
+    return query as any;
+  }
+
+  test("select id, title, userId from posts", () => {
+    const tableName = getReturnTypeFromSelect("select id, title, userId from posts");
+    assertType<{
+      id: number;
+      title: string;
+      userId: number;
+    }>(tableName);
+  });
+
+  test("select id, postId from comments", () => {
+    const tableName = getReturnTypeFromSelect("select id, postId from comments");
+    assertType<{
+      id: number;
+      postId: number;
     }>(tableName);
   });
 });
--- src/select.ts
+++ src/select.ts
@@ -4,6 +4,25 @@ export type ExtractTableName<Query extends string> = Query extends `select ${inf
     ? TableName
     : never;
 
+type ExtractColumns<Query extends string> = Query extends `select ${infer Columns} from ${infer _TableName}`
+  ? Columns
+  : Query extends `SELECT ${infer Columns} FROM ${infer _TableName}`
+    ? Columns
+    : never;
+
 export type GetTableType<Query extends string, Tables> = Tables[ExtractTableName<Query> extends keyof Tables
   ? ExtractTableName<Query>
   : never];
+
+type Split<S extends string, Delimiter extends string> = S extends `${infer T}${Delimiter}${infer U}`
+  ? [T, ...Split<U, Delimiter>]
+  : [S];
+
+type MapColumnsToTypes<Table, Columns> = Columns extends string
+  ? Pick<Table, Extract<keyof Table, Split<Columns, ", ">[number]>>
+  : never;
+
+export type InferReturnTypeFromSelect<Query extends string, Tables> = MapColumnsToTypes<
+  GetTableType<Query, Tables>,
+  ExtractColumns<Query>
+>;
