1df30ec378aef3e2ad7b28e1d0618895bfbf8966
Michael Haar <michael.haar92@gmail.com>
2024-01-26 09:26:09 +0100

    implement select-expressions

---
+++ src/select-expression.test-d.ts
@@ -0,0 +1,74 @@
+import { test, describe, expectTypeOf } from "vitest";
+import { ParseSelectExpressions, SelectExpressionToTableColumnTypeMap } from "./select-expression";
+
+describe("ParseSelectExpressions", () => {
+  function parseSelectExpressions<Query extends string>(query: Query): ParseSelectExpressions<Query> {
+    return query as any;
+  }
+
+  test("*", () => {
+    const selectExpressions = parseSelectExpressions("*");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["*"]>();
+  });
+
+  test("col1, col2", () => {
+    const selectExpressions = parseSelectExpressions("col1, col2");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["col1", "col2"]>();
+  });
+
+  test("tbl_name.*", () => {
+    const selectExpressions = parseSelectExpressions("tbl_name.*");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["tbl_name.*"]>();
+  });
+
+  test("tbl_name.col1, tbl_name.col2", () => {
+    const selectExpressions = parseSelectExpressions("tbl_name.col1, tbl_name.col2");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["tbl_name.col1", "tbl_name.col2"]>();
+  });
+});
+
+describe("SelectExpressionToTableColumnTypeMap", () => {
+  type TestTables = {
+    users: {
+      id: number;
+      name: string;
+    };
+    posts: {
+      id: number;
+      title: string;
+      body: string;
+      userId: number;
+    };
+  };
+
+  function selectExpressionToTableColumnTypeMap<SE extends string>(
+    se: SE
+  ): SelectExpressionToTableColumnTypeMap<SE, ["users"], TestTables> {
+    return se as any;
+  }
+
+  test("*", () => {
+    const selectExpression = selectExpressionToTableColumnTypeMap("*");
+    expectTypeOf(selectExpression).toMatchTypeOf<TestTables["users"]>();
+  });
+
+  test("id", () => {
+    const selectExpression = selectExpressionToTableColumnTypeMap("id");
+    expectTypeOf(selectExpression).toMatchTypeOf<number>();
+  });
+
+  test("name", () => {
+    const selectExpression = selectExpressionToTableColumnTypeMap("name");
+    expectTypeOf(selectExpression).toMatchTypeOf<string>();
+  });
+
+  test("users.*", () => {
+    const selectExpression = selectExpressionToTableColumnTypeMap("users.*");
+    expectTypeOf(selectExpression).toMatchTypeOf<TestTables["users"]>();
+  });
+
+  test("users.id", () => {
+    const selectExpression = selectExpressionToTableColumnTypeMap("users.id");
+    expectTypeOf(selectExpression).toMatchTypeOf<number>();
+  });
+});
---
+++ src/select-expression.ts
@@ -0,0 +1,58 @@
+/**
+ * Official MySQL documentation for SELECT:
+ * https://dev.mysql.com/doc/refman/8.0/en/select.html
+ *
+ * Unfortunately, the documentation is not very clear on the syntax of the `select_expr` part.
+ *
+ * From the documentation, it seems like `select_expr` is specified as one of the following:
+ *
+ * ```
+ * select_expressions:
+ *     select_expr [, select_expr] ...
+ *
+ * select_expr:
+ *    `*`
+ *   | tbl_name.`*` [[AS] alias]
+ *   | col_name [[AS] alias]
+ *   | tbl_name.col_name [AS alias]
+ *   | TODO: add Functions (COUNT, SUM, AVG, etc.), Expressions (1 + 1, etc.) and Things like `DISTINCT`?
+ * ```
+ *
+ *
+ *
+ * Not supported ATM:
+ *  - AS alias
+ *  - Functions (COUNT, SUM, AVG, etc.)
+ *  - Expressions (1 + 1, etc.)
+ *  - Things like `DISTINCT`
+ */
+
+export type ParseSelectExpressions<Query extends string> = Query extends `${infer SelectExpressions}, ${infer Rest}`
+  ? [SelectExpressions, ...ParseSelectExpressions<Rest>]
+  : [Query];
+
+export type SelectExpressionToTableColumnTypeMap<
+  SelectExpression,
+  TableNames extends string[],
+  AllTablesObj,
+> = SelectExpression extends "*"
+  ? TableNames[0] extends keyof AllTablesObj
+    ? AllTablesObj[TableNames[0]]
+    : never
+  : SelectExpression extends `${infer TableName}.*`
+    ? TableName extends keyof AllTablesObj
+      ? AllTablesObj[TableName]
+      : never
+    : SelectExpression extends `${infer TableName}.${infer ColumnName}` // TODO: support nested?
+      ? TableName extends keyof AllTablesObj
+        ? ColumnName extends keyof AllTablesObj[TableName]
+          ? AllTablesObj[TableName][ColumnName]
+          : never
+        : never
+      : SelectExpression extends `${infer ColumnName}`
+        ? TableNames[0] extends keyof AllTablesObj
+          ? ColumnName extends keyof AllTablesObj[TableNames[0]]
+            ? AllTablesObj[TableNames[0]][ColumnName]
+            : never
+          : never
+        : never;
