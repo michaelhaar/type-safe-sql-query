2e28b13fa191c1cf7fa51e77718f81ea12a3ade5
Michael Haar <michael.haar92@gmail.com>
2024-01-26 11:21:27 +0100

    Proof of concept

--- src/select.test-d.ts
+++ src/select.test-d.ts
@@ -1,111 +1,99 @@
-import { assertType, test, describe, expectTypeOf } from "vitest";
-import type { ExtractTableName, InferReturnTypeFromSelect, GetTableType } from "./select";
+import { test, describe, expectTypeOf } from "vitest";
+import type {
+  GetSelectExpressions,
+  GetTableNames,
+  InferReturnTypeFromSelectStatement,
+  ParseSelectStatement,
+} from "./select";
 
-type TestTables = {
-  users: {
-    id: number;
-    name: string;
-  };
-  posts: {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  };
-  comments: {
-    id: number;
-    body: string;
-    postId: number;
-  };
-};
-
-describe("ExtractTableName", () => {
-  function getTableName<Query extends string>(query: Query): ExtractTableName<Query> {
+describe("ParseSelectStatement", () => {
+  function parseSelectStatement<Query extends string>(query: Query): ParseSelectStatement<Query> {
     return query as any;
   }
 
-  test("select * from users", () => {
-    const tableName = getTableName("select * from users");
-    assertType<"users">(tableName);
+  test("SELECT * FROM tbl_name", () => {
+    const selectStatement = parseSelectStatement("SELECT * FROM tbl_name");
+    expectTypeOf(selectStatement).toMatchTypeOf<{ selectExpressionsString: "*"; tableReferencesString: "tbl_name" }>();
   });
 
-  test("SELECT * FROM users", () => {
-    const tableName = getTableName("SELECT * FROM users");
-    assertType<"users">(tableName);
-  });
-
-  test("select id from users", () => {
-    const tableName = getTableName("select id from users");
-    assertType<"users">(tableName);
-  });
-
-  test("select id, name from users", () => {
-    const tableName = getTableName("select id, name from users");
-    assertType<"users">(tableName);
+  test("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2", () => {
+    const selectStatement = parseSelectStatement("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2");
+    expectTypeOf(selectStatement).toMatchTypeOf<{
+      selectExpressionsString: "col1, col2";
+      tableReferencesString: "tbl_name1 JOIN tbl_name2";
+    }>();
   });
 });
 
-describe("GetTableType", () => {
-  function getTableType<Query extends string>(query: Query): GetTableType<Query, TestTables> {
+describe("GetSelectExpressions", () => {
+  function getSelectExpressions<Query extends string>(query: Query): GetSelectExpressions<Query> {
     return query as any;
   }
 
-  test("select * from users", () => {
-    const tableName = getTableType("select * from users");
-    assertType<{
-      id: number;
-      name: string;
-    }>(tableName);
+  test("SELECT * FROM tbl_name", () => {
+    const selectExpressions = getSelectExpressions("SELECT * FROM tbl_name");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["*"]>();
   });
 
-  test("SELECT * FROM posts", () => {
-    const tableName = getTableType("select * from posts");
-    assertType<{
+  test("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2", () => {
+    const selectExpressions = getSelectExpressions("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2");
+    expectTypeOf(selectExpressions).toMatchTypeOf<["col1", "col2"]>();
+  });
+});
+
+describe("GetTableNames", () => {
+  function getTableNames<Query extends string>(query: Query): GetTableNames<Query> {
+    return query as any;
+  }
+
+  test("SELECT * FROM tbl_name", () => {
+    const tableNames = getTableNames("SELECT * FROM tbl_name");
+    expectTypeOf(tableNames).toMatchTypeOf<["tbl_name"]>();
+  });
+
+  test("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2", () => {
+    const tableNames = getTableNames("SELECT col1, col2 FROM tbl_name1 JOIN tbl_name2");
+    expectTypeOf(tableNames).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+});
+
+describe("InferReturnTypeFromSelectStatement", () => {
+  type TestTables = {
+    users: {
+      id: number;
+      name: string;
+    };
+    posts: {
       id: number;
       title: string;
       body: string;
       userId: number;
-    }>(tableName);
-  });
+    };
+    comments: {
+      id: number;
+      body: string;
+      postId: number;
+    };
+  };
 
-  test("SELECT * FROM invalidTableName", () => {
-    const tableName = getTableType("select * from invalidTableName");
-    expectTypeOf(tableName).toMatchTypeOf<never>();
-  });
-});
-
-describe("InferReturnTypeFromSelect", () => {
-  function getReturnTypeFromSelect<Query extends string>(query: Query): InferReturnTypeFromSelect<Query, TestTables> {
+  function inferReturnTypeFromSelectStatement<Query extends string>(
+    query: Query
+  ): InferReturnTypeFromSelectStatement<Query, TestTables> {
     return query as any;
   }
 
-  test("select * from users", () => {
-    const tableName = getReturnTypeFromSelect("select * from users");
-    assertType<{
-      id: number;
-      name: string;
-    }>(tableName);
+  test("SELECT * FROM users", () => {
+    const result = inferReturnTypeFromSelectStatement("SELECT * FROM users");
+    expectTypeOf(result).toMatchTypeOf<TestTables["users"]>();
   });
 
-  test("select id, title, userId from posts", () => {
-    const tableName = getReturnTypeFromSelect("select id, title, userId from posts");
-    assertType<{
-      id: number;
-      title: string;
-      userId: number;
-    }>(tableName);
+  test("SELECT id, name FROM users", () => {
+    const result = inferReturnTypeFromSelectStatement("SELECT id, name FROM users");
+    expectTypeOf(result).toMatchTypeOf<{ id: number; name: string }>();
   });
 
-  test("SELECT id, postId FROM comments", () => {
-    const tableName = getReturnTypeFromSelect("SELECT id, postId FROM comments");
-    assertType<{
-      id: number;
-      postId: number;
-    }>(tableName);
-  });
-
-  test("SELECT * FROM invalidTableName", () => {
-    const tableName = getReturnTypeFromSelect("select * from invalidTableName");
-    expectTypeOf(tableName).toMatchTypeOf<never>();
+  test("SELECT users.id, posts.title FROM users JOIN posts", () => {
+    const result = inferReturnTypeFromSelectStatement("SELECT users.id, posts.title FROM users JOIN posts");
+    expectTypeOf(result).toMatchTypeOf<{ id: number; title: string }>();
   });
 });
--- src/select.ts
+++ src/select.ts
@@ -1,30 +1,82 @@
-export type ExtractTableName<Query extends string> = Query extends `select ${infer _Columns} from ${infer TableName}`
-  ? TableName
-  : Query extends `SELECT ${infer _Columns} FROM ${infer TableName}`
-    ? TableName
+/**
+ * Official MySQL documentation for SELECT:
+ * https://dev.mysql.com/doc/refman/8.0/en/select.html
+ *
+ * ```
+ * SELECT
+ *     [ALL | DISTINCT | DISTINCTROW ]
+ *     [HIGH_PRIORITY]
+ *     [STRAIGHT_JOIN]
+ *     [SQL_SMALL_RESULT] [SQL_BIG_RESULT] [SQL_BUFFER_RESULT]
+ *     [SQL_NO_CACHE] [SQL_CALC_FOUND_ROWS]
+ *     select_expr [, select_expr] ...
+ *     [into_option]
+ *     [FROM table_references
+ *       [PARTITION partition_list]]
+ *     [WHERE where_condition]
+ *     [GROUP BY {col_name | expr | position}, ... [WITH ROLLUP]]
+ *     [HAVING where_condition]
+ *     [WINDOW window_name AS (window_spec)
+ *         [, window_name AS (window_spec)] ...]
+ *     [ORDER BY {col_name | expr | position}
+ *       [ASC | DESC], ... [WITH ROLLUP]]
+ *     [LIMIT {[offset,] row_count | row_count OFFSET offset}]
+ *     [into_option]
+ *     [FOR {UPDATE | SHARE}
+ *         [OF tbl_name [, tbl_name] ...]
+ *         [NOWAIT | SKIP LOCKED]
+ *       | LOCK IN SHARE MODE]
+ *     [into_option]
+ *
+ * into_option: {
+ *     INTO OUTFILE 'file_name'
+ *         [CHARACTER SET charset_name]
+ *         export_options
+ *   | INTO DUMPFILE 'file_name'
+ *   | INTO var_name [, var_name] ...
+ * }
+ * ```
+ */
+
+/**
+ * Assumptions:
+ * - Keywords are always uppercase
+ * - Whitespace is always a single space
+ * - TODO: Add Notes on Quotes
+ */
+
+/**
+ * Not supported:
+ * - TODO: add keywords
+ */
+
+import {
+  ParseSelectExpressions,
+  PickWithSanitizedSelectExpressions,
+  SanitizeSelectExpressions,
+} from "./select-expression";
+import { ParseTableReferences } from "./table-references";
+
+export type ParseSelectStatement<Query extends string> =
+  Query extends `SELECT ${infer SelectExpressionsString} FROM ${infer TableReferencesString}`
+    ? {
+        selectExpressionsString: SelectExpressionsString;
+        tableReferencesString: TableReferencesString;
+      }
     : never;
 
-type ExtractColumns<Query extends string> = Query extends `select ${infer Columns} from ${infer _TableName}`
-  ? Columns
-  : Query extends `SELECT ${infer Columns} FROM ${infer _TableName}`
-    ? Columns
-    : never;
+export type GetTableNames<Query extends string> = ParseTableReferences<
+  ParseSelectStatement<Query>["tableReferencesString"]
+>;
+export type GetSelectExpressions<Query extends string> = ParseSelectExpressions<
+  ParseSelectStatement<Query>["selectExpressionsString"]
+>;
+export type GetSanitizedSelectExpressions<Query extends string> = SanitizeSelectExpressions<
+  GetSelectExpressions<Query>,
+  GetTableNames<Query>[0]
+>;
 
-export type GetTableType<Query extends string, Tables> = Tables[ExtractTableName<Query> extends keyof Tables
-  ? ExtractTableName<Query>
-  : never];
-
-type Split<S extends string, Delimiter extends string> = S extends `${infer T}${Delimiter}${infer U}`
-  ? [T, ...Split<U, Delimiter>]
-  : [S];
-
-type MapColumnsToTypes<Table, Columns> = Columns extends "*"
-  ? Table
-  : Columns extends string
-    ? Pick<Table, Extract<keyof Table, Split<Columns, ", ">[number]>>
-    : never;
-
-export type InferReturnTypeFromSelect<Query extends string, Tables> = MapColumnsToTypes<
-  GetTableType<Query, Tables>,
-  ExtractColumns<Query>
+export type InferReturnTypeFromSelectStatement<Query extends string, Tables> = PickWithSanitizedSelectExpressions<
+  GetSanitizedSelectExpressions<Query>,
+  Tables
 >;
