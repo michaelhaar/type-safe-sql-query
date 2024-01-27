6af21953b2a924ec1ce9ec6c2fd0eb79a45a4498
Michael Haar <michael.haar92@gmail.com>
2024-01-27 19:21:17 +0100

    fix tests
    see: https://github.com/vitest-dev/vitest/issues/5021

--- src/table-references.test-d.ts
+++ src/table-references.test-d.ts
@@ -5,101 +5,99 @@ function parseTableReferences<TR extends string>(tr: TR): ParseTableReferences<T
   return tr as any;
 }
 
-describe("Single table reference", () => {
-  describe("Basic", () => {
-    test("tbl_name", () => {
-      const tf = parseTableReferences("tbl_name");
-      expectTypeOf(tf).toMatchTypeOf<["tbl_name"]>();
-    });
+describe("Basic", () => {
+  test("tbl_name", () => {
+    const tf = parseTableReferences("tbl_name");
+    expectTypeOf(tf).toMatchTypeOf<["tbl_name"]>();
+  });
+});
+
+describe("Single Joins", () => {
+  test("tbl_name1 JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
   });
 
-  describe("Single Joins", () => {
-    test("tbl_name1 JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 INNER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 INNER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 CROSS JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 CROSS JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 STRAIGHT_JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 STRAIGHT_JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 LEFT JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 LEFT JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 RIGHT JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 RIGHT JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 LEFT OUTER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 LEFT OUTER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 RIGHT OUTER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 RIGHT OUTER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL LEFT JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL LEFT JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL RIGHT JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL RIGHT JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL LEFT OUTER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL LEFT OUTER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL RIGHT OUTER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL RIGHT OUTER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL INNER JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL INNER JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
-
-    test("tbl_name1 NATURAL JOIN tbl_name2", () => {
-      const jt = parseTableReferences("tbl_name1 NATURAL JOIN tbl_name2");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
-    });
+  test("tbl_name1 INNER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 INNER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
   });
 
-  describe("Multiple Joins", () => {
-    test("tbl_name1 JOIN tbl_name2 JOIN tbl_name3", () => {
-      const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 JOIN tbl_name3");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
-    });
+  test("tbl_name1 CROSS JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 CROSS JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
 
-    test("tbl_name1 JOIN tbl_name2 INNER JOIN tbl_name3", () => {
-      const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 INNER JOIN tbl_name3");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
-    });
+  test("tbl_name1 STRAIGHT_JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 STRAIGHT_JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
 
-    test("tbl_name1 LEFT JOIN tbl_name2 CROSS JOIN tbl_name3", () => {
-      const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 CROSS JOIN tbl_name3");
-      expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
-    });
+  test("tbl_name1 LEFT JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 LEFT JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 RIGHT JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 RIGHT JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 LEFT OUTER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 LEFT OUTER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 RIGHT OUTER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 RIGHT OUTER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL LEFT JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL LEFT JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL RIGHT JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL RIGHT JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL LEFT OUTER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL LEFT OUTER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL RIGHT OUTER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL RIGHT OUTER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL INNER JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL INNER JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+
+  test("tbl_name1 NATURAL JOIN tbl_name2", () => {
+    const jt = parseTableReferences("tbl_name1 NATURAL JOIN tbl_name2");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2"]>();
+  });
+});
+
+describe("Multiple Joins", () => {
+  test("tbl_name1 JOIN tbl_name2 JOIN tbl_name3", () => {
+    const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 JOIN tbl_name3");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
+  });
+
+  test("tbl_name1 JOIN tbl_name2 INNER JOIN tbl_name3", () => {
+    const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 INNER JOIN tbl_name3");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
+  });
+
+  test("tbl_name1 LEFT JOIN tbl_name2 CROSS JOIN tbl_name3", () => {
+    const jt = parseTableReferences("tbl_name1 JOIN tbl_name2 CROSS JOIN tbl_name3");
+    expectTypeOf(jt).toMatchTypeOf<["tbl_name1", "tbl_name2", "tbl_name3"]>();
   });
 });
 
